88a6ea40bf66b0aec7d2985699c6b4c2af708c8f
chika0801 <88967758+chika0801@users.noreply.github.com>
2023-08-12 12:55:39 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -26,4 +26,4 @@ speed() {
 curl -sLo sp https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz && tar -xzf sp && rm -r sp speedtest.5 speedtest.md && chown root:root speedtest
 mkdir -p /root/speedtest-cli && mv ./speedtest /root/speedtest-cli
 clear && printf "%-18s%-18s%-20s%-12s\n" " Node Name" "Upload Speed" "Download Speed" "Latency"
-speed && rm -r speedtest-cli && rm -r /root/.config/ookla
+speed && rm -r /root/speedtest-cli && rm -r /root/.config/ookla
